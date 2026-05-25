8739b2130a53aff040db128c5fcbb01f7bc59ccf
Aadhaar Verma <aadhaar80@gmail.com>
2026-05-25 09:31:03 +0530

    Add: max-water-container - submission-0

---
+++ Data Structures & Algorithms/max-water-container/submission-0.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int maxArea(vector<int>& heights) {
+        int max_area = 0;
+        int area;
+        for(int i=0;i<heights.size()-1;i++){
+            for(int j=i+1;j<heights.size();j++){
+                area = min(heights[i], heights[j])*(j-i);
+                max_area = max(area, max_area);
+            }
+        }
+        return max_area;
+    }
+};
