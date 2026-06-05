8d285ac80a1d8e71d76cc041d37299d89aaec796
Aadhaar Verma <aadhaar80@gmail.com>
2026-06-05 10:51:45 +0530

    Add: search-2d-matrix - submission-0

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-0.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int m = matrix.size();
+        int n = matrix[0].size();
+        
+        for(int i=0;i<m;i++){
+            if(matrix[i][n-1] == target) return true;
+            else if(matrix[i][n-1] < target) continue;
+            else if(matrix[i][n-1] > target){
+                int left = 0;
+                int right = n-1;
+                while(left<=right){
+                    int mid = left + (right - left) / 2;
+
+                    if(matrix[i][mid] == target) return true;
+                    else if(matrix[i][mid] < target) left = mid+1;
+                    else right = mid-1;
+                }
+                return false;
+            }
+        }
+        return false;
+    }
+};
