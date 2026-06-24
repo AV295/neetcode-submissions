149a8d5271a76d8627167c62c6a089d5f8b73bf7
Aadhaar Verma <aadhaar80@gmail.com>
2026-06-25 00:10:37 +0530

    Add: boats-to-save-people - submission-1

---
+++ Data Structures & Algorithms/boats-to-save-people/submission-1.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int numRescueBoats(vector<int>& people, int limit) {
+        sort(people.begin(), people.end());
+        int n = people.size();
+        int count = 0;
+        if(limit < people[n-1]) return 0;
+        int left = 0, right = n-1;
+        while(left<=right){
+            if((people[left]+people[right])>limit){
+                count++;
+                right--;
+            }else{
+                count++;
+                left++;
+                right--;
+            }
+        }
+        return count;
+    }
+};
